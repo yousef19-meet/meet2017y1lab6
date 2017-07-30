0b3b5aeb1bada4684817b856a83d984d89fbbfb5
yousef19-meet <yousef19@meet.mit.edu>
2017-07-30 18:15:51 +0300

    Stopping here

---
+++ LIsTs.py
@@ -0,0 +1,3 @@
+a=['she',"sells",'sea',"shells",'by',"the",'sea',"shore"]
+b="selfish shellfish"
+c=[1,1,2,3,5,8,13]
---
+++ stringfun.py
@@ -0,0 +1,5 @@
+def string_test(s):
+    if len(s) > 2 and s[0] == s[-1]:
+        return True
+    else:
+        return False
