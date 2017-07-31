e5fc43bade525c5521fb09a043d17a2b5456e438
yousef19-meet <yousef19@meet.mit.edu>
2017-07-31 15:49:49 +0300

    Stopping here

--- Turlte.py
+++ Turlte.py
@@ -1,5 +1,49 @@
 import turtle
 
+def up ():
+    print("you pressed up!")
+    global direction
+    direction=UP
+    print(direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y+10)
+    print(turtle.pos())
+    
+def down ():
+    print("you pressed down!")
+    direction=DOWN
+    print(direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos())
+
+    
+def right ():
+    print("you pressed right!")
+    direction=RIGHT
+    print(direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x+10,y)
+    print(turtle.pos())
+
+    
+def left ():
+    print("you pressed left!")
+    direction=LEFT
+    print(direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x-10,y)
+    print(turtle.pos())
+
+    
 UP_ARROW="Up"
 
 LEFT_ARROW="Left"
@@ -16,26 +60,17 @@ LEFT=2
 RIGHT=3
 direction=UP
 
-def up ():
-    print("you pressed up!")
-    global direction
-    direction=UP
-    print(direction)
+turtle.onkeypress(up,UP_ARROW)
+
+turtle.onkeypress(down,DOWN_ARROW)
+
+turtle.onkeypress(left,LEFT_ARROW)
     
-def down ():
-    print("you pressed down!")
-    direction=DOWN
-    print(direction)
-    
-def right ():
-    print("you pressed right!")
-    direction=RIGHT
-    print(direction)
-    
-def left ():
-    print("you pressed left!")
-    direction=LEFT
-    print(direction)
+turtle.onkeypress(right,RIGHT_ARROW)
+
+turtle.listen()
+
+
     
 turtle.mainloop()
 
